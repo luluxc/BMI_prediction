2ccc8b190c711eb30d0ed46b48a5d0f52f6f18dc
luluxc <133820078+luluxc@users.noreply.github.com>
2023-05-18 17:44:23 -0500

    Update bmi_prediction_app.py

--- bmi_prediction_app.py
+++ bmi_prediction_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import cv2
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+#from tensorflow.keras.models import load_model
 import tensorflow_probability as tfp
 import keras.utils as image
 from keras_vggface.utils import preprocess_input
