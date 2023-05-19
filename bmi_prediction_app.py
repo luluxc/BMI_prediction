5d4dee8a8d6f38a212342c1d95055eb43c470226
luluxc <133820078+luluxc@users.noreply.github.com>
2023-05-18 23:27:29 -0500

    Update bmi_prediction_app.py

--- bmi_prediction_app.py
+++ bmi_prediction_app.py
@@ -23,9 +23,9 @@ def pearson_corr(y_test, y_pred):
 
 # with custom_object_scope({'pearson_corr': pearson_corr}):
 #   model = load_model('My_model_vgg16.h5')
-  
+custom_objects = {'pearson_corr':pearson_corr}
 
-model = load_model('My_model_vgg16.h5', custom_objects={'pearson_corr':pearson_corr})
+model = load_model('My_model_vgg16.h5', custom_objects=custom_objects)
 
 def predict_class(image, model):
   img = image.copy()
