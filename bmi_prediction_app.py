3572049754a394d155c2fc9909be9bacb0d06599
luluxc <133820078+luluxc@users.noreply.github.com>
2023-05-18 22:38:11 -0500

    Update bmi_prediction_app.py

--- bmi_prediction_app.py
+++ bmi_prediction_app.py
@@ -19,7 +19,7 @@ def pearson_corr(y_test, y_pred):
   return corr
 
 def custom_object_scope(custom_objects):
-  return keras_utils.CustomObjectScope(custom_objects)
+  return keras.utils.CustomObjectScope(custom_objects)
 
 with custom_object_scope({'pearson_corr': pearson_corr}):
   model = load_model('My_model_vgg16.h5')
