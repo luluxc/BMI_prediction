b2c534803fd83d0072ef0189bfa5e1c9dcc36791
luluxc <133820078+luluxc@users.noreply.github.com>
2023-05-18 21:24:23 -0500

    Update bmi_prediction_app.py

--- bmi_prediction_app.py
+++ bmi_prediction_app.py
@@ -46,67 +46,71 @@ def process_img(file_image):
   pred_image = image
   return pred_image
 
-if 'photo' not in st.session_state:
-  st.session_state['photo'] = 'Not done'
-
-st.set_page_config(layout="wide")
-
-st.markdown("""
-<style>
-.big-font {
-    font-size:150px !important;
-}
-</style>
-""", unsafe_allow_html=True)
-
-st.markdown('<p class="big-font">BMI Prediction 📸</p>', unsafe_allow_html=True)
-#st.title('*BMI prediction 📸*')
-col2, col3 = st.columns([2,1])
 
 def change_photo_state():
   st.session_state['photo'] = 'Done'
 
+def main():
+  if 'photo' not in st.session_state:
+    st.session_state['photo'] = 'Not done'
 
-upload_img = col3.file_uploader('Upload a photo 🖼', on_change=change_photo_state)
-file_image = col2.camera_input('Take a pic of you 😊', on_change=change_photo_state)
+  st.set_page_config(layout="wide")
 
-if st.session_state['photo'] == 'Done':
-  process_bar3 = col3.progress(0)
+  st.markdown("""
+  <style>
+  .big-font {
+      font-size:150px !important;
+  }
+  </style>
+  """, unsafe_allow_html=True)
 
-  process_bar2 = col2.progress(0)
+  st.markdown('<p class="big-font">BMI Prediction 📸</p>', unsafe_allow_html=True)
+  #st.title('*BMI prediction 📸*')
+  col2, col3 = st.columns([2,1])
 
-  if file_image:
-    for process in range(100):
-      time.sleep(0.05)
-      process_bar2.progress(process+1)
-    col2.success('Taken the photo sucessfully!')
-    pred_camera = process_img(file_image)
-    st.image(pred_camera, caption='Predicted photo')
-    image = Image.fromarray(pred_camera)
-    # Convert the PIL Image to bytes
-    image_bytes = io.BytesIO()
-    image.save(image_bytes, format='PNG')
-    image_bytes = image_bytes.getvalue()
-    download_img = st.download_button(
-      label='Download image', 
-      data=image_bytes,
-      file_name=file_image.name.split('.')[0] + '_bmi.png',
-      mime="image/png")
-  elif upload_img:
-    for process in range(100):
-      time.sleep(0.05)
-      process_bar3.progress(process+1)
-    col3.success('Uploaded the photo sucessfully!')
-    pred_upload = process_img(upload_img)
-    st.image(pred_upload, caption='Predicted photo')
-    image = Image.fromarray(pred_upload)
-    # Convert the PIL Image to bytes
-    image_bytes = io.BytesIO()
-    image.save(image_bytes, format='PNG')
-    image_bytes = image_bytes.getvalue()
-    st.write('Download the predicted image if you want!')
-    download_img = st.download_button(
-      label='Download image', 
-      data=image_bytes,
-      file_name=upload_img.name.split('.')[0]  + '_bmi.png',
-      mime="image/png")
+  upload_img = col3.file_uploader('Upload a photo 🖼', on_change=change_photo_state)
+  file_image = col2.camera_input('Take a pic of you 😊', on_change=change_photo_state)
+
+  if st.session_state['photo'] == 'Done':
+    process_bar3 = col3.progress(0)
+
+    process_bar2 = col2.progress(0)
+
+    if file_image:
+      for process in range(100):
+        time.sleep(0.05)
+        process_bar2.progress(process+1)
+      col2.success('Taken the photo sucessfully!')
+      pred_camera = process_img(file_image)
+      st.image(pred_camera, caption='Predicted photo')
+      image = Image.fromarray(pred_camera)
+      # Convert the PIL Image to bytes
+      image_bytes = io.BytesIO()
+      image.save(image_bytes, format='PNG')
+      image_bytes = image_bytes.getvalue()
+      download_img = st.download_button(
+        label='Download image', 
+        data=image_bytes,
+        file_name=file_image.name.split('.')[0] + '_bmi.png',
+        mime="image/png")
+    elif upload_img:
+      for process in range(100):
+        time.sleep(0.05)
+        process_bar3.progress(process+1)
+      col3.success('Uploaded the photo sucessfully!')
+      pred_upload = process_img(upload_img)
+      st.image(pred_upload, caption='Predicted photo')
+      image = Image.fromarray(pred_upload)
+      # Convert the PIL Image to bytes
+      image_bytes = io.BytesIO()
+      image.save(image_bytes, format='PNG')
+      image_bytes = image_bytes.getvalue()
+      st.write('Download the predicted image if you want!')
+      download_img = st.download_button(
+        label='Download image', 
+        data=image_bytes,
+        file_name=upload_img.name.split('.')[0]  + '_bmi.png',
+        mime="image/png")
+ 
+if __name__=='__main__':
+    main()
